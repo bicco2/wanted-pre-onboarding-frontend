7cfd7c3bd2a0531fb7a11ba7d8f2e79f767fba20
bicco2 <jhb_0921@naver.com>
2023-04-12 04:10:29 +0900

    Fix : delete unable code

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { UseInputValidation } from "../../hooks/useInputValidation";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
