2f54e334269004885d8dfaf3533c3ad2856ed7e8
bicco2 <jhb_0921@naver.com>
2023-04-12 04:09:49 +0900

    Add signin api

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,76 @@
-import React from "react";
+import React, { useState } from "react";
+import { UseInputValidation } from "../../hooks/useInputValidation";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-export default function index() {
-  return <div>signin</div>;
+import { API_BASE_URL } from "../../constants/constants";
+import "./index.scss";
+
+type FormData = {
+  email: String;
+  password: String;
+};
+
+export default function SignInPage() {
+  const emailCustomHook = UseInputValidation(/@/g);
+  const pwCustomHook = UseInputValidation(/^.{8,}$/);
+
+  const navigate = useNavigate();
+
+  const handleBtn = async () => {
+    try {
+      await axios
+        .post(
+          `${API_BASE_URL}/auth/signin`,
+          {
+            email: emailCustomHook.value,
+            password: pwCustomHook.value,
+          } as FormData,
+          { headers: { "Content-Type": "application/json" } }
+        )
+        .then((res) => {
+          console.log(res.data.access_token);
+          localStorage.setItem("ACCESS_TOKEN", res.data.access_token);
+        });
+      navigate("/todo");
+    } catch (error) {
+      alert("error");
+      console.error(error);
+    }
+  };
+  return (
+    <div className="container">
+      <h1 className="title">로그인</h1>
+      <div className="form">
+        <label>
+          <span>이메일</span>
+          <input
+            data-testid="email-input"
+            type="email"
+            onChange={emailCustomHook.handleChange}
+            placeholder="이메일을 입력해주세요"
+          />
+          {!emailCustomHook.isValid && <p>'@'가 포함되어야 합니다.</p>}
+        </label>
+        <label>
+          <span>비밀번호</span>
+          <input
+            data-testid="password-input"
+            type="password"
+            onChange={pwCustomHook.handleChange}
+            placeholder="비밀번호를 입력해주세요"
+          />
+          {!pwCustomHook.isValid && <p>8자리 이상이여야 합니다.</p>}
+        </label>
+        <button
+          data-testid="signin-button"
+          type="submit"
+          disabled={!pwCustomHook.isValid || !emailCustomHook.isValid}
+          onClick={handleBtn}
+        >
+          로그인하기
+        </button>
+      </div>
+    </div>
+  );
 }
